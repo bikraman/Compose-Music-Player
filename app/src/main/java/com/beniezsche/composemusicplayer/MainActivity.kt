225a4dcc9b15bde2b5d906abb7df6685fda78a85
bikraman <bikramankarmakar10@gmail.com>
2024-02-24 18:52:28 +0530

    fetching songs data from api and populating list

--- app/src/main/java/com/beniezsche/composemusicplayer/MainActivity.kt
+++ app/src/main/java/com/beniezsche/composemusicplayer/MainActivity.kt
@@ -14,8 +14,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,6 +26,8 @@ import com.beniezsche.composemusicplayer.models.SongModel
 import com.beniezsche.composemusicplayer.models.SongsResponse
 import com.beniezsche.composemusicplayer.networking.RetrofitClient
 import com.beniezsche.composemusicplayer.ui.theme.ComposeMusicPlayerTheme
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -31,49 +36,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val songList = ArrayList<SongModel>()
-
-        RetrofitClient.getClient().getSongs().enqueue(object: Callback<SongsResponse> {
-            override fun onResponse(call: Call<SongsResponse>, response: Response<SongsResponse>) {
-                if (response.isSuccessful) {
-                    response.body()?.data?.let {
-                        songList.addAll(it)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<SongsResponse>, t: Throwable) {
-
-            }
-
-        })
-
-//        for(i in 1..100) {
-//            val songModel = SongModel()
-//            songModel.id = i
-//            songModel.name = "Song $i"
-//            songModel.artist = "Artist $i"
-//            songList.add(songModel)
-//        }
-
         setContent {
 
-            val itemList = remember { mutableListOf(null) }
+            val itemList = remember { mutableStateListOf<SongModel>() }
+
+            LaunchedEffect(key1 = "hello", block = {
+                RetrofitClient.getClient().getSongs().enqueue(object: Callback<SongsResponse> {
+                    override fun onResponse(call: Call<SongsResponse>, response: Response<SongsResponse>) {
+                        if (response.isSuccessful) {
+                            response.body()?.data?.let {
+                                itemList.addAll(it)
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<SongsResponse>, t: Throwable) {
+
+                    }
+
+                })
+            } )
             ComposeMusicPlayerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LazyColumn {
-//                        items()
+                    LazyColumn (modifier = Modifier.fillMaxSize()) {
                         items(
-                            count = songList.size,
+                            count = itemList.size,
                             key = {
-                                songList[it].id
+                                itemList[it].id
                             },
                             itemContent = {index: Int ->
-                                SongItem(albumCoverIcon = "nothing" , name = songList[index].name!!, artistName = songList[index].artist!! )
+                                SongItem(albumCoverIcon = itemList[index].cover , name = itemList[index].name, artistName = itemList[index].artist)
                             }
                         )
                     }
@@ -84,14 +80,16 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun SongItem(albumCoverIcon: String, name: String, artistName: String) {
 
     Row(modifier = Modifier.padding(20.dp)){
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = name.toString(),
-            modifier = Modifier.size(48.dp))
+        GlideImage(model = "https://cms.samespace.com/assets/$albumCoverIcon",
+            contentDescription = name,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(48.dp)
+        )
         Column(modifier = Modifier.padding(start = 10.dp)) {
             Text(text = name, fontSize = 17.sp)
             Text(text = artistName, fontSize = 15.sp)
